49132065e6bd3f63c5fa21720c1e8e8bbb28581c
Guilian <guilian.bahno@laposte.net>
2021-12-22 22:59:46 +0100

    ajout du projet cookle en php + modification des images fullsize + essai de logo licorne

---
+++ Projets/GSB/GSB_GestFrais/Console/GSBFraisModel/Buisness/FicheFrais.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSBFraisModel.Buisness
+{
+    class FicheFrais
+    {
+        private Visiteurs unVisiteur;
+        private string _mois;
+        private Etat unEtat;
+        private decimal _MontantValide;
+        private int _nbjustificatif;
+        private DateTime _dateModif;
+
+        internal Visiteurs UnVisiteur
+        {
+            get
+            {
+                return unVisiteur;
+            }
+
+            set
+            {
+                unVisiteur = value;
+            }
+        }
+
+        public string Mois
+        {
+            get
+            {
+                return _mois;
+            }
+
+            set
+            {
+                _mois = value;
+            }
+        }
+
+        internal Etat UnEtat
+        {
+            get
+            {
+                return unEtat;
+            }
+
+            set
+            {
+                unEtat = value;
+            }
+        }
+
+        public decimal MontantValide
+        {
+            get
+            {
+                return _MontantValide;
+            }
+
+            set
+            {
+                _MontantValide = value;
+            }
+        }
+
+        public int Nbjustificatif
+        {
+            get
+            {
+                return _nbjustificatif;
+            }
+
+            set
+            {
+                _nbjustificatif = value;
+            }
+        }
+
+        public DateTime DateModif
+        {
+            get
+            {
+                return _dateModif;
+            }
+
+            set
+            {
+                _dateModif = value;
+            }
+        }
+    }
+}
